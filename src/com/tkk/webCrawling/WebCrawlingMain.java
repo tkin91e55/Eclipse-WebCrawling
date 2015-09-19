42a311d16321ea97ca45192a62810482de0c7db1
tkingless <t.kingless@yahoo.com>
2015-09-20 01:28:13 +0800

    update main.java

--- src/com/tkk/webCrawling/WebCrawlingMain.java
+++ src/com/tkk/webCrawling/WebCrawlingMain.java
@@ -1,10 +1,12 @@
 package com.tkk.webCrawling;
-import com.tkk.webCrawling.webCrawler;
+
+import com.tkk.webCrawling.webCrawler.ECTutorCrawler;
 
 public class WebCrawlingMain {
 
 	public static void main(String[] args) {
 		
+		ECTutorCrawler ecTutorWorker = ECTutorCrawler.GetInstance();
 		System.out.println("Program main runned to LAST line!");
 
 	}
