ed86dc4e7710537c30dcd81e93e7c2338178e6fd
tkingless <t.kingless@yahoo.com>
2015-09-19 15:52:19 +0800

    remane Main run file

--- src/WebCrawlingMain.java
+++ src/WebCrawlingMain.java
@@ -27,7 +27,7 @@ import org.apache.commons.collections4.*;
 import org.apache.commons.collections4.map.MultiValueMap;
 import org.apache.commons.csv.*;
 
-public class CrawlECTutor {
+public class WebCrawlingMain {
 	//Params
 	public static String CONFIG_FILE = "config.csv";
 	public static String URL_KEY = "WC_URL";
