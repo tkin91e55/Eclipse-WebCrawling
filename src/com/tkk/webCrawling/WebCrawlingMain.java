dfec488b53efe83aefa7ab277f2a898c56314586
tkingless <t.kingless@yahoo.com>
2015-09-25 13:14:15 +0800

    pre add L4Tutor

--- src/com/tkk/webCrawling/WebCrawlingMain.java
+++ src/com/tkk/webCrawling/WebCrawlingMain.java
@@ -6,21 +6,19 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.*;
 
 public class WebCrawlingMain {
 
 	public static void main(String[] args) {
 
-		// TODO: DB flushing
-		Crawlee_DB DB = Crawlee_DB.GetInstance();
+		//DB flushing
+		Crawlee_DB.GetInstance();
 
 		// declared all the websites
 		BaseCrawler ecTutorWorker = ECTutorCrawler.GetInstance();
 
 		// WAIT, until constructors finish and have websites get their
 		// board indexes
-
 		List<Crawlee> eCrawlees = ecTutorWorker.getCrawlees();
 
 		synchronized (eCrawlees) {
@@ -37,7 +35,7 @@ public class WebCrawlingMain {
 		ConcurrencyMachine.GetInstance().RegisterQueue(eCrawlees);
 		ConcurrencyMachine.GetInstance().InvokeQueue();
 
-		// TODO: WAIT, until crawled and crawlee mature, write to same DB file
+		//WAIT, until crawled and crawlee mature, write to same DB file
 		// need to lock the log file
 		Date runTime = new Date();
 
@@ -60,7 +58,7 @@ public class WebCrawlingMain {
 
 		System.out.println("[ECTutor crawlees] size 4: " + ecTutorWorker.getCrawlees().size());
 
-		// TODO: WAIT, until writing DB file, write result file (this is
+		// WAIT, until writing DB file, write result file (this is
 		// postprocessing)
 		ecTutorWorker.PostProcessAction();
 
