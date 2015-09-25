0021b2af82d1d6016124f0193be932eb980f4df2
tkingless <t.kingless@yahoo.com>
2015-09-25 14:31:15 +0800

    both togehter workable

--- src/com/tkk/webCrawling/WebCrawlingMain.java
+++ src/com/tkk/webCrawling/WebCrawlingMain.java
@@ -15,24 +15,38 @@ public class WebCrawlingMain {
 		Crawlee_DB.GetInstance();
 
 		// declared all the websites
+		BaseCrawler ecTutorWorker = ECTutorCrawler.GetInstance();
 		BaseCrawler l4TutorWorker = L4TutorCrawler.GetInstance();
 
 		// WAIT, until constructors finish and have websites get their
 		// board indexes
-		List<Crawlee> eCrawlees = l4TutorWorker.getCrawlees();
+		List<Crawlee> eCrawlees = ecTutorWorker.getCrawlees();
+		List<Crawlee> lCrawlees = l4TutorWorker.getCrawlees();
 
 		synchronized (eCrawlees) {
 			try {
-				l4TutorWorker.StartRun();
+				ecTutorWorker.StartRun();
 				eCrawlees.wait();
 
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
+		
+		synchronized (lCrawlees) {
+			try {
+				l4TutorWorker.StartRun();
+				lCrawlees.wait();
 
-		System.out.println("[ECTutor crawlees] size 2: " + l4TutorWorker.getCrawlees().size());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		System.out.println("[ECTutor crawlees] size 2: " + ecTutorWorker.getCrawlees().size());
+		System.out.println("[L4Tutor crawlees] size 2: " + l4TutorWorker.getCrawlees().size());
 		ConcurrencyMachine.GetInstance().RegisterQueue(eCrawlees);
+		ConcurrencyMachine.GetInstance().RegisterQueue(lCrawlees);
 		ConcurrencyMachine.GetInstance().InvokeQueue();
 
 		//WAIT, until crawled and crawlee mature, write to same DB file
@@ -52,14 +66,28 @@ public class WebCrawlingMain {
 			}
 		}
 		
-		System.out.println("[ECTutor crawlees] size 3: " + l4TutorWorker.getCrawlees().size());
-
+		for (Iterator<Crawlee> crwl_iter = lCrawlees.iterator(); crwl_iter.hasNext();) {
+			Crawlee crwl = crwl_iter.next();
+			try {
+				if (Crawlee_DB.GetInstance().LookUpFromDB(crwl, runTime)) {
+					crwl_iter.remove();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("[ECTutor crawlees] size 3: " + ecTutorWorker.getCrawlees().size());
+		System.out.println("[L4Tutor crawlees] size 3: " + l4TutorWorker.getCrawlees().size());
+		ecTutorWorker.FilterByCritAction();
 		l4TutorWorker.FilterByCritAction();
 
-		System.out.println("[ECTutor crawlees] size 4: " + l4TutorWorker.getCrawlees().size());
-
+		System.out.println("[ECTutor crawlees] size 4: " + ecTutorWorker.getCrawlees().size());
+		System.out.println("[L4Tutor crawlees] size 4: " + l4TutorWorker.getCrawlees().size());
+		
 		// WAIT, until writing DB file, write result file (this is
 		// postprocessing)
+		ecTutorWorker.PostProcessAction();
 		l4TutorWorker.PostProcessAction();
 
 		System.out.println("Program main runned to LAST line!");
